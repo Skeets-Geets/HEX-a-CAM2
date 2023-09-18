08ffdd5e72760675c668686d8a2370675af8a480
Ethan <77466469+Skeets-Geets@users.noreply.github.com>
2023-09-18 16:42:16 -0600

    Initial Commit

---
+++ HEX-a-CAM/HEX_a_CAMApp.swift
@@ -0,0 +1,20 @@
+//
+//  HEX_a_CAMApp.swift
+//  HEX-a-CAM
+//
+//  Created by GEET on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct HEX_a_CAMApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
