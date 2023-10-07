1f91b249c9ed3b02c1f2399303bc2ca56c810475
Ethan <77466469+Skeets-Geets@users.noreply.github.com>
2023-10-06 21:51:10 -0600

    Update MyColorsMenu.swift
    creates the functionality of the mycolorsmenu relationship with the my colors button

--- MyColorsMenu.swift
+++ MyColorsMenu.swift
@@ -12,41 +12,49 @@ import SpriteKit
 struct MyColorsMenu: View {
     @Binding var isMenuVisible: Bool
     @Binding var hexagonScale: CGFloat
-    let gifFrames: [Image] 
+    let gifFrames: [Image] // This array should contain all frames of your GIF as Image objects
     @State private var currentFrameIndex = 0
     @State private var timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    @State var menuScale: CGFloat = 0.01  // Add this state variable to handle scaling
 
     var body: some View {
-           VStack {
-               HStack {
-                   Spacer()
-                   Button("Close") {
-                       withAnimation {
-                           isMenuVisible.toggle()
-                           hexagonScale = 1.0
-                       }
-                   }
-                   .padding()
-               }
+        VStack {
+            HStack {
+                Spacer()
+                Button("Close") {
+                    withAnimation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0.5)) {
+                        self.menuScale = 0.01  // Scale down when the close button is pressed
+                        isMenuVisible.toggle()
+                        hexagonScale = 1.0
+                    }
+                }
+                .padding()
+            }
 
-               Text("My Colors")
-                   .font(.largeTitle)
-                   .bold()
+            Text("My Colors")
+                .font(.largeTitle)
+                .bold()
 
-               LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 20), count: 3), spacing: 20) {
-                   ForEach(0..<9) { _ in
-                       SpinningHexagonScene()
-                           .frame(width: 100, height: 100)
-                           .background(Color.clear)
-                   }
-               }
-               Spacer()
-           }
-           .padding()
-           .background(VisualEffectView(effect: UIBlurEffect(style: .dark)))
-           .cornerRadius(20)
-       }
-   }
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 20), count: 3), spacing: 20) {
+                ForEach(0..<9) { _ in
+                    SpinningHexagonScene()
+                        .frame(width: 100, height: 100)
+                        .background(Color.clear)
+                }
+            }
+            Spacer()
+        }
+        .padding()
+        .background(VisualEffectView(effect: UIBlurEffect(style: .dark)))
+        .cornerRadius(20)
+        .scaleEffect(menuScale)
+        .onAppear {
+            withAnimation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0.5)) {
+                self.menuScale = 1.0  // Scale up when the view appears
+            }
+        }
+    }
+}
 
 struct MyColorsMenu_Previews: PreviewProvider {
     static var previews: some View {
